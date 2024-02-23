921e0dd34d93b9093d33c7dceed392e387c4d820
jadenwilliams <jaw98x@gmail.com>
2024-02-23 09:52:57 -0500

    cskd

--- src/main/java/com/example/javaapp/LoginController.java
+++ src/main/java/com/example/javaapp/LoginController.java
@@ -1,11 +1,21 @@
 package com.example.javaapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
+    public TextField EnterGym;
+    public Button Enter;
+
+
+
+
 
 
     @FXML
---
+++ src/main/java/com/example/javaapp/ManagerController.java
@@ -0,0 +1,12 @@
+package com.example.javaapp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+
+public class ManagerController {
+    public TableView Chart;
+    public TableView Chart2;
+    public Text Poorstanding;
+    public Text Customers;
+}
