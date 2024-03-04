efe0c911edb4a57c9b336a5648c950ee2215afc8
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2024-03-04 13:22:28 -0500

    login

--- src/main/java/com/example/javaapp/CustomController.java
+++ src/main/java/com/example/javaapp/CustomController.java
@@ -3,6 +3,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CustomController {
     public Label PFP;
     public Label Standing;
@@ -15,9 +19,29 @@ public class CustomController {
     }
     public void uploadPic(){
     }
-    public void initialize(){
-        for (User singleUser: User.allUsers) {
+    public void initialize() throws IOException{
+        String filePath = "currentUser.txt";
 
+        // Try-with-resources statement to automatically close the resources
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                for (User singleUser: User.allUsers) {
+                    String ID =  singleUser.getGymID();
+                    if(ID.equals(line)) {
+                        System.out.println(singleUser);
+
+                    }
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
     }
 }
--- src/main/java/com/example/javaapp/LoginController.java
+++ src/main/java/com/example/javaapp/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.javaapp;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +12,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.*;
 
 public class LoginController {
     @FXML
@@ -30,16 +33,35 @@ public class LoginController {
 
 
     public void login(ActionEvent event) throws Exception {
-        root = FXMLLoader.load(getClass().getResource("customer.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        String Code = EnterGym.getText();
+        for (User singleUser: User.allUsers) {
+               String ID =  singleUser.getGymID();
+               if(ID.equals(Code)){
+                   save(Code);
+                   root = FXMLLoader.load(getClass().getResource("customer.fxml"));
+                   stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                   scene = new Scene(root);
+                   stage.setScene(scene);
+                   stage.show();
+               }else{
+                   System.out.println("Invalid Login");
+               }
+        }
+
+
     }
     public void restore() {
 
     }
-    public void save() {
+    public void save(String LoginCode) throws IOException {
+        String filePath = "currentUser.txt";
+        try (FileWriter fileWriter = new FileWriter(filePath);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+             bufferedWriter.write(LoginCode);
+             System.out.println("String has been written to the file.");
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
