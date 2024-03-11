ef1a906e3e501b61b02eca7fad9cd6f7c2378015
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2024-03-11 13:41:47 -0400

    login

--- src/main/java/com/example/javaapp/CustomController.java
+++ src/main/java/com/example/javaapp/CustomController.java
@@ -1,21 +1,45 @@
 package com.example.javaapp;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class CustomController {
     public Label PFP;
     public Label Standing;
     public Label AOV;
+    public Text Welcomeback;
 
     public Button Uploadpicture;
     public Button LOGOUT;
+    public Stage stage;
+    public Scene scene;
+    public Parent root;
+    public void logout(ActionEvent event) throws IOException{
+        String filePath = "currentUser.txt";
+        try (FileWriter fileWriter = new FileWriter(filePath);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write("");
+            System.out.println("String has been written to the file.");
 
-    public void logout(){
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     public void uploadPic(){
     }
@@ -32,6 +56,9 @@ public class CustomController {
                     String ID =  singleUser.getGymID();
                     if(ID.equals(line)) {
                         System.out.println(singleUser);
+                        String Name = singleUser.getVisitorname();
+                        Welcomeback.setText("Welcome Back: " +Name);
+
 
                     }
                 }
