a192648458942a2b760cddae4e6222045b6b362b
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2024-03-12 14:27:08 -0400

    manager

--- src/main/java/com/example/javaapp/ManagerController.java
+++ src/main/java/com/example/javaapp/ManagerController.java
@@ -8,26 +8,23 @@ import javafx.scene.text.Text;
 
 
 public class ManagerController {
-    public TableView<User> Chart;
-    public TableView Chart2;
+    public TableView Chart;
+
     public Text Poorstanding;
     public Text Customers;
-    public TableColumn<User, String> GymID;
-    public TableColumn <User, String>Visitorname;
-    public TableColumn <User, String>VisitorPicture;
-    public TableColumn <User, String>amountofVisits;
-    public TableColumn<User, String> Standing;
-    public TableColumn<User, String> SignedIn;
 
 
-public void initalize() throws Exception {
 
-    GymID.setCellValueFactory(new PropertyValueFactory<User, String>("GymID"));
-    Visitorname.setCellValueFactory(new PropertyValueFactory<User, String>("Name"));
-    VisitorPicture.setCellValueFactory(new PropertyValueFactory<User, String>("Picture"));
-    amountofVisits.setCellValueFactory(new PropertyValueFactory<User, String>("NumofVis"));
-    Standing.setCellValueFactory(new PropertyValueFactory<User, String>("Standing"));
-    SignedIn.setCellValueFactory(new PropertyValueFactory<User, String>("IsSignedIn"));
+public void initialize() throws Exception {
+System.out.println("Ddd");
+    TableColumn<User, String> gymid = new TableColumn<>("GymID");
+    gymid.setCellValueFactory(new PropertyValueFactory<User, String>("GymID"));
+
+    TableColumn<User, String> name = new TableColumn<>("Visitorname");
+    name.setCellValueFactory(new PropertyValueFactory<User, String>("Visitorname"));
+
+    Chart.getColumns().add(gymid);
+    Chart.getColumns().add(name);
 
     Chart.getItems().addAll(User.allUsers);
 
