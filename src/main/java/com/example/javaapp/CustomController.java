d6f7e00d9fba00bdfe2f3e6a814ae6ecadb3e20e
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2024-03-22 10:25:24 -0400

    image

--- src/main/java/com/example/javaapp/CustomController.java
+++ src/main/java/com/example/javaapp/CustomController.java
@@ -7,10 +7,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
+import javax.imageio.ImageIO;
 import java.io.*;
 
 public class CustomController {
@@ -36,15 +38,43 @@ public class CustomController {
         stage.setScene(scene);
         stage.show();
     }
+    ImageView imageView = new ImageView(new Image("your_image_file_path.jpg"));
+
 
     public void onActionUpload() {
         FileChooser fileChooser = new FileChooser();
 
         File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            saveImageToFile(imageView.getImage(), selectedFile);
+        }
     }
 
     ;
+    private void saveImageToFile(Image image, File file) {
+        try {
+            OutputStream outputStream = new FileOutputStream(file);
+            // Use the appropriate image format when writing to the file
+            javafx.scene.image.PixelReader reader = image.getPixelReader();
+            java.awt.image.BufferedImage bufferedImage = new java.awt.image.BufferedImage(
+                    (int) image.getWidth(),
+                    (int) image.getHeight(),
+                    java.awt.image.BufferedImage.TYPE_INT_ARGB);
 
+            // Copy the pixels of the Image to the BufferedImage
+            for (int y = 0; y < image.getHeight(); y++) {
+                for (int x = 0; x < image.getWidth(); x++) {
+                    bufferedImage.setRGB(x, y, reader.getArgb(x, y));
+                }
+            }
+
+            // Write the BufferedImage to the output stream
+            ImageIO.write(bufferedImage, "png", outputStream);
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void initialize() throws IOException {
         currentUser = User.allUsers.get(0);
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.javaapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.javaapp to javafx.fxml;
